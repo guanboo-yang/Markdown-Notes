ce37cf3d01fc9b4ca849032e19d9c9260e3455a1
Guan-Bo Yang <b09902064@ntu.edu.tw>
2023-08-06 00:31:00 +0800

    v0.1.0 📂 Fix Next Config
    Signed-off-by: Guan-Bo Yang <b09902064@ntu.edu.tw>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  basePath: '/markdown-notes',
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
